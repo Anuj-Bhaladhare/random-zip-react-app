c646eaf465eb3b4db0d19c7c263c5f8caac7dd5c
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-05 12:30:38 +0530

    all functionality is done

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,53 +1,51 @@
- import React, { useEffect, useState } from "react";
- import Spinner from "./Spinner";
 
- 
- const Tag = () => {
+import React, { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
-  const[gifs, setGifs] = useState('');
-  const[loader, setLoader] = useState(false);
-  const[tag, setTag] = useState('');
+const Tag = () => {
+  const [gifs, setGifs] = useState('');
+  const [loader, setLoader] = useState(false);
+  const [tag, setTag] = useState('');
 
   const API_KEY = "furZgPcPwGG7iZEQX5ApqEuayUzEqCAq";
   const url = "https://api.giphy.com/v1/gifs/random";
-  
-  const fetchData = async() => {
 
+  const fetchData = async () => {
     setLoader(true);
-    try{
+    try {
       const getData = await fetch(`${url}?api_key=${API_KEY}&tag=${tag}`);
-      const {data} = await getData.json();
-      setGifs(data.images.downsized.url);
+      const { data } = await getData.json();
+      const imageSource = data.images.downsized.url;
+      setGifs(imageSource);
+      console.log(imageSource);
+    } catch (err) {
+      console.log(err);
     }
-    catch(err) {
-       console.log(err);
-    }
-    setLoader(false)
+    setLoader(false);
   }
- 
-  useEffect( () => {
-    fetchData();
-  })
 
-  const searchHandaler = () => {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const searchHandler = () => {
     fetchData();
   }
 
   return (
+    <div>
+      <h2>Search Random Gifs</h2>
       <div>
-          <h2>Search Randon Gifs</h2>
-          <div>
-            {
-              loader ? ( <Spinner /> ) : (<img src={gifs}/>)
-            }  
-          </div>
-          <input 
-            type="text"
-            onChange={ (event) => setTag(event.target.value) }
-            value = {tag}
-          />
-          <button onClick={searchHandaler}>Search</button>
+        {loader ? (<Spinner />) : (<img src={gifs} alt="Random Gif" />)}
       </div>
+      <input
+        type="text"
+        onChange={(event) => setTag(event.target.value)}
+        value={tag}
+      />
+      <button onClick={searchHandler}>Search</button>
+    </div>
   )
 }
+
 export default Tag;
